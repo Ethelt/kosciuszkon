903f7c7239f1f7ed216c8e062a7f540be09f2b42
Bartłomiej Hada <bartlomiej.hada@gmail.com>
2025-05-31 14:21:57 +0200

    shared

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { shared } from "shared";
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ app.use(express.json());
 
 // Basic route
 app.get("/", (req, res) => {
-  res.json({ message: "Server is running!" });
+  res.json({ message: shared });
 });
 
 // Start server
